a64688726f0b52da33def7b85f132eb932fb1cca
Andy Whitfield <andy@andywhitfield.me.uk>
2020-05-08 09:16:33 +0100

    More UI tweaks
    Most of the skeleton is there, time to work on the backend a bit now

--- SmallMealPlan.Web/wwwroot/js/smallmealplan.js
+++ SmallMealPlan.Web/wwwroot/js/smallmealplan.js
@@ -3,8 +3,20 @@ function smpInitialise() {
 
     $("ul.smp-planner-list").sortable({
         handle: '.smp-planner-list-meal-drag-handle',
-        isValidTarget: function($item, container) {
+        isValidTarget: function(item, container) {
             return container.el[0].className.includes('smp-planner-list-meal');
+        },
+        onDrop: function(item, container, _super, event) {
+            _super(item, container, event);
+
+            // TODO: callback to update meal planner...
+            let mealMoved = item.attr('data-meal');
+            let newDate = item.parent('ul').attr('data-day');
+            let prevMeal = item.prev('li').attr('data-meal');
+            if (!prevMeal) prevMeal = -1;
+            let nextMeal = item.next('li').attr('data-meal');
+            if (!nextMeal) nextMeal = -1;
+            console.log('moved ' + mealMoved + ' to day ' + newDate + ' between ' + prevMeal + ' and ' + nextMeal);
         }
     });
 
