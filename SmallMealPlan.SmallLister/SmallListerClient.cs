27e7920ff04c5ff0c17a7bbda8ad05b2a86fd3b0
Andy Whitfield <andy@andywhitfield.me.uk>
2023-06-25 16:24:25 +0100

    Changed webhook signature to accept a list of events

--- SmallMealPlan.SmallLister/SmallListerClient.cs
+++ SmallMealPlan.SmallLister/SmallListerClient.cs
@@ -49,8 +49,7 @@ namespace SmallMealPlan.SmallLister
 
         public async Task RegisterWebhookAsync(string refreshToken, Uri webhookBaseUri, string userId)
         {
-            await CallApiAsync(refreshToken, HttpMethod.Delete, "/api/v1/webhook/ListChange");
-            await CallApiAsync(refreshToken, HttpMethod.Delete, "/api/v1/webhook/ListItemChange");
+            await UnregisterWebhookAsync(refreshToken);
             await CallApiAsync(refreshToken, HttpMethod.Post, "/api/v1/webhook", new { Webhook = new Uri(webhookBaseUri, $"api/webhook/{userId}/smalllister/list"), WebhookType = "ListChange" });
             await CallApiAsync(refreshToken, HttpMethod.Post, "/api/v1/webhook", new { Webhook = new Uri(webhookBaseUri, $"api/webhook/{userId}/smalllister/listitem"), WebhookType = "ListItemChange" });
         }
--- SmallMealPlan.Web/Controllers/SmallListerWebhookApiController.cs
+++ SmallMealPlan.Web/Controllers/SmallListerWebhookApiController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -16,17 +18,17 @@ public class SmallListerWebhookApiController : ControllerBase
 
     [HttpPost("~/api/webhook/{userid}/smalllister/list")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public ActionResult HandleListChange(int userid, ListChange request)
+    public ActionResult HandleListChange(int userid, IEnumerable<ListChange> request)
     {
-        _logger.LogInformation($"Received list change webhook for userid: {userid}; listid: {request.ListId}; event: {request.Event}");
+        _logger.LogInformation($"Received list change webhook for userid: {userid}: [{string.Join(',', request.Select(lc => $"listid: {lc.ListId}; event: {lc.Event}"))}]");
         return Ok();
     }
 
     [HttpPost("~/api/webhook/{userid}/smalllister/listitem")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public ActionResult HandleListItemChange(int userid, ListItemChange request)
+    public ActionResult HandleListItemChange(int userid, IEnumerable<ListItemChange> request)
     {
-        _logger.LogInformation($"Received list item change webhook for userid: {userid}; listid: {request.ListId}; listitemid: {request.ListItemId}; event: {request.Event}");
+        _logger.LogInformation($"Received list item change webhook for userid: {userid}: [{string.Join(',', request.Select(lc => $"listid: {lc.ListId}; listitemid: {lc.ListItemId}; event: {lc.Event}"))}]");
         return Ok();
     }
 }
